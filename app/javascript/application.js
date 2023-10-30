4e550d57d8fb5573e13642604d180cde79134c80
hadzicamer <hadzic.amer@outlook.com>
2023-10-30 11:23:11 +0100

    .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require_tree .
-import 'app/javascript/channels/search_channel.js'
+import '../javascript/channels/search_channel.js'
 import "@hotwired/turbo-rails"
+require("channels")
+
+// Then you can include your custom JavaScript files. For example:
+require("./channels/search_channel.js")
+
---
+++ public/assets/application-aa8b9666295b847f8e61723d6a62c9c074b0be6a282665722d78d3c0c39e8e1d.js
@@ -0,0 +1,57 @@
+// Action Cable provides the framework to deal with WebSockets in Rails.
+// You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
+
+import { createConsumer } from "@rails/actioncable"
+
+export default createConsumer();
+// Import all the channels to be used by Action Cable
+import "channels/search_channel";
+import consumer from "channels/consumer"
+
+export const subscription = consumer.subscriptions.create("SearchChannel", {
+  connected() {
+    console.log("Connected to search channel")
+  },
+
+  disconnected() {
+    console.log("Disconnected to search channel")
+  },
+
+  received(data) {
+    const { query, count } = data;
+    
+    // Update your UI here based on the received query and count
+  }
+  
+});
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application };
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.textContent = "Hello World!"
+  }
+};
+// Import and register all your controllers from the importmap under controllers/*
+
+import { application } from "controllers/application"
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
+
+// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+// lazyLoadControllersFrom("controllers", application);
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import 'app/javascript/channels/search_channel.js'
+import "@hotwired/turbo-rails";
