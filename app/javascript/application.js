17dab4ed79e9837d4e26a8f68302680e4f5b3823
hadzicamer <hadzic.amer@outlook.com>
2023-10-30 10:02:15 +0100

    .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require_tree .
-//= require channels/search_channel
+import "channels/search_channel"
 import "@hotwired/turbo-rails"
